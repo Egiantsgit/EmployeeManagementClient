ddc353c095e93e2f3b2383e2951b2e1291ed6d04
AKHILA <akhila.prodduturi95@gmail.com>
2018-07-25 16:55:42 -0500

    home-footer component changed

--- src/app/home-footer/home-footer.component.ts
+++ src/app/home-footer/home-footer.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
+import {FormControl, FormGroup, Validators,FormBuilder} from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import {NgForm} from '@angular/forms';
+import {Http} from '@angular/http';
 
 @Component({
   selector: 'app-home-footer',
   templateUrl: './home-footer.component.html',
   styleUrls: ['./home-footer.component.css']
-})
+})     
 export class HomeFooterComponent implements OnInit {
 
-  constructor() { }
+  formErrors={'email':''};
+  form = new FormGroup({
+    email: new FormControl(),
+  });
 
-  ngOnInit() {
-  }
+   constructor(private http: Http) {
+   }   
+ 
+   ngOnInit() {
+     
+    }
+    
+  EmailSubscribe(){
+    this.formErrors['email']= '';
+    let post:any={title:this.form.value.email};
+    
+      if((this.form.value.email==null) ||( this.form.value.email.untouched && this.form.value.email.dirty)|| (this.form.value.email=="")){
+        this.formErrors['email']= 'email is required';
+        return;
+      }
+  
+      this.http.post('  ', JSON.stringify(post))
+      .subscribe(response=>{
+        console.log(this.form.value.email);
+          console.log(response.json());
+         },
+      err => {
+        console.log("Error occured");
+      }
+           );
+         
+     }
 
-}
+    }
